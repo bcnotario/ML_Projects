7255df34a8880ee051a53e216d6c5b8aec8022b0
Brandon N <37309728+bcnotario@users.noreply.github.com>
2018-03-15 12:34:11 -0700

    Update MixtureModel.R

--- DigitClassifier/R_scripts/MixtureModel.R
+++ DigitClassifier/R_scripts/MixtureModel.R
@@ -1,76 +1,43 @@
 #Mixture Model Digit Classifier
-#Inputs: mixtures (Total Mixtures), number (Digit)
-#Data Files: digits.rdata
+#Inputs: mixtures (Total Mixtures)
+#Data Files: digits.rdata (Training & Test Data)
 
-#####SECTION1 - Training & Test Data Set-Up
-load("/Users/Documents/digits.rdata")
-num.class <- dim(training.data)[1]  # Number of classes; 0-9 (true labels)
-num.training <- dim(training.data)[2]  # Number of training data per class; 500 total
-d <- prod(dim(training.data)[3:4])
-dim(training.data) <- c(num.class * num.training, d) # Reshape training data to vector per i
-training.label <- rep(0:9, num.training) # Labels of training data.
-num.test <- dim(test.data)[2] # Number of test data; 1000 total
-dim(test.data) <- c(num.class * num.test, d) # Reshape training data to vector per i
-test.label <- rep(0:9, num.test) # Labels of test data
-
-#####SECTION2 - Digit Mixture Model Function
-#Input total mixture models and digit; output final pi_m, mu_mj, log-likelihood table
-Digit.MM <- function(mixtures,number){
-  #Initial Parameters
-  M <- mixtures; digit <- number; N <- num.training
-  digit.train <- training.data[which(training.label==digit),]
+Digit.MM <- function(mixtures){
   
-  #Initial BMM Parameters, Cluster (mixture model), Log-Likelihood Table
-  clu.0 <- sample(M,N,replace=TRUE)
-  iter <- matrix(c(0,0),1,2)
-  colnames(iter) <- c("Iteration","Log-Lik")
-  pi.mat <- matrix(0,M,1)
-  mu.mat <- matrix(0,M,d)
-  for(m in 1:M){
-    pi.mat[m,1] <- (length(clu.0[which(clu.0==m)])+1)/(N+M)
-    mu.mat[m,] <- (colSums(digit.train[which(clu.0==m),])+1)/(dim(digit.train[which(clu.0==m),])[1]+2)
-  }
-  
-  #E-Step Initial
-  gam.mat <- matrix(0,N,M)
-  gam.tmp <- matrix(0,N,M)
-  gam.exp <- matrix(0,N,M)
-  for(i in 1:N){
-    #Calculate Bernoulli P(X_i|mu_m)
-    for(m in 1:M){
-      pX.im <- vector()
-      for(j in 1:d){pX.im[j] <- (mu.mat[m,j])^(digit.train[i,j]*1)*(1-mu.mat[m,j])^(1-digit.train[i,j]*1)}
-      gam.tmp[i,m] <- log(pi.mat[m]*prod(pX.im))
-    }
+  #Setup Training Data
+  load("/Users/Documents/digits.rdata")
+  nclass <- dim(training.data)[1] #Total Number Classes
+  ntrain <- dim(training.data)[2]  #Total Training Data
+  d <- prod(dim(training.data)[3:4]) #Total Data Dimensions
+  dim(training.data) <- c(nclass * ntrain, d) # Reshape training data to vector per i
+  trainX <- training.data #Digit Data
+  trainY <- rep(0:9, ntrain) #Digit Label
     
-    #Find the max pi_m*P(X_i|mu_m) and calculate the new gamma probability
-    l.max <- max(gam.tmp[i,])
-    for(m in 1:M){gam.exp[i,m] <- exp(gam.tmp[i,m]-l.max)}
-    for(m in 1:M){gam.mat[i,] <- gam.exp[i,]/sum(gam.exp[i,])}
-  }
+  #Setup Test Data
+  ntest <- dim(test.data)[2] # Number of test data; 1000 total
+  dim(test.data) <- c(nclass * ntest, d) # Reshape training data to vector per i
+  testX <- test.data
+  testY <- rep(0:9, ntest) # Labels of test data
   
-  #Log-Likelihood (Initial)
-  Q.mat <- matrix(0,N,M)
-  for(i in 1:N){
-    for(m in 1:M){
-      Q.mat[i,m] <- gam.mat[i,m]*(log(pi.mat[m])+t(digit.train[i,])%*%log(mu.mat[m,])+t(1-digit.train[i,])%*%log(1-mu.mat[m,]))
-    }
-  }
-  iter[1,2]<-sum(Q.mat)+sum(log(pi.mat))+log(factorial(2*M-1))+sum(log(mu.mat[m,]))+sum(log(1-mu.mat[m,]))+400*log(6)
+  #Initial Parameters & Model List
+  M <- mixtures; N <- ntrain; Mod.list <- list()
   
-  #Loop until Log-Likelihood Difference <=1
-  converge <- 500
-  while(converge>1){
+  #Training Each Digit
+  for(digit in 1:nclass){
+    if(digit==10){digit.train <- trainX[which(trainY==0),]} else {digit.train <- trainX[which(trainY==digit),]}
     
-    #M-Step Loop
+    #Initial BMM Parameters, Cluster (mixture model), Log-Likelihood Table
+    clu.0 <- sample(M,N,replace=TRUE)
+    iter <- matrix(c(0,0),1,2)
+    colnames(iter) <- c("Iteration","Log-Lik")
     pi.mat <- matrix(0,M,1)
     mu.mat <- matrix(0,M,d)
     for(m in 1:M){
-      pi.mat[m,1] <- (sum(gam.mat[,m])+1)/(N+M)
-      mu.mat[m,] <- (t(digit.train)%*%gam.mat[,m]+1)/(sum(gam.mat[,m])+2)
+      pi.mat[m,1] <- (length(clu.0[which(clu.0==m)])+1)/(N+M)
+      mu.mat[m,] <- (colSums(digit.train[which(clu.0==m),])+1)/(dim(digit.train[which(clu.0==m),])[1]+2)
     }
     
-    #E-Step Loop
+    #E-Step Initial
     gam.mat <- matrix(0,N,M)
     gam.tmp <- matrix(0,N,M)
     gam.exp <- matrix(0,N,M)
@@ -88,86 +55,111 @@ Digit.MM <- function(mixtures,number){
       for(m in 1:M){gam.mat[i,] <- gam.exp[i,]/sum(gam.exp[i,])}
     }
     
-    #Log-Likelihood
+    #Log-Likelihood (Initial)
     Q.mat <- matrix(0,N,M)
     for(i in 1:N){
       for(m in 1:M){
         Q.mat[i,m] <- gam.mat[i,m]*(log(pi.mat[m])+t(digit.train[i,])%*%log(mu.mat[m,])+t(1-digit.train[i,])%*%log(1-mu.mat[m,]))
       }
     }
+    iter[1,2]<-sum(Q.mat)+sum(log(pi.mat))+log(factorial(2*M-1))+sum(log(mu.mat[m,]))+sum(log(1-mu.mat[m,]))+400*log(6)
     
-    iter <- rbind(iter,c(max(iter[,1]+1),sum(Q.mat)+sum(log(pi.mat))+log(factorial(2*M-1))+sum(log(mu.mat[m,]))+sum(log(1-mu.mat[m,]))+400*log(6)))
-    
-    #Converges when Log-Likelihood Difference <1
-    converge <- iter[max(iter[,1])+1,2]-iter[max(iter[,1]),2]
-  }
-  return(list(Iter=iter,Pi=pi.mat,Mu=mu.mat,Gamma=gam.mat))
-}
-
-#####SECTION3 - Digit 0-9; Mixture Models = {5}; Training Time 984 seconds of 10K training images
-Sys.time()
-mod1.5 <- Digit.MM(5,1)
-mod2.5 <- Digit.MM(5,2)
-mod3.5 <- Digit.MM(5,3)
-mod4.5 <- Digit.MM(5,4)
-mod5.5 <- Digit.MM(5,5)
-mod6.5 <- Digit.MM(5,6)
-mod7.5 <- Digit.MM(5,7)
-mod8.5 <- Digit.MM(5,8)
-mod9.5 <- Digit.MM(5,9)
-mod0.5 <- Digit.MM(5,0)
-Sys.time()
-
-
-#####SECTION4 - Digit 3 & 5 Classification
-#Testing Data
-Test.mat <- matrix(0,num.test*10,12)
-colnames(Test.mat) <- c("1","2","3","4","5","6","7","8","9","0","True","Test")
-Test.mat[,11] <- rep(c(10,1:9),1000)
-
-Sys.time()
-for(i in 1:(num.test*10)){
-  Digit.mat <- matrix(0,10,5)
-  for(m in 1:5){
-    pX.1 <- vector(); pX.2 <- vector(); pX.3 <- vector(); pX.4 <- vector(); pX.5 <- vector()
-    pX.6 <- vector(); pX.7 <- vector(); pX.8 <- vector(); pX.9 <- vector(); pX.0 <- vector()
-    for(j in 1:d){
-      pX.1[j] <- mod1.5$Mu[m,j]^(test.data[i,j])*(1-mod1.5$Mu[m,j])^(1-test.data[i,j])
-      pX.2[j] <- mod2.5$Mu[m,j]^(test.data[i,j])*(1-mod2.5$Mu[m,j])^(1-test.data[i,j])
-      pX.3[j] <- mod3.5$Mu[m,j]^(test.data[i,j])*(1-mod3.5$Mu[m,j])^(1-test.data[i,j])
-      pX.4[j] <- mod4.5$Mu[m,j]^(test.data[i,j])*(1-mod4.5$Mu[m,j])^(1-test.data[i,j])
-      pX.5[j] <- mod5.5$Mu[m,j]^(test.data[i,j])*(1-mod5.5$Mu[m,j])^(1-test.data[i,j])
-      pX.6[j] <- mod6.5$Mu[m,j]^(test.data[i,j])*(1-mod6.5$Mu[m,j])^(1-test.data[i,j])
-      pX.7[j] <- mod7.5$Mu[m,j]^(test.data[i,j])*(1-mod7.5$Mu[m,j])^(1-test.data[i,j])
-      pX.8[j] <- mod8.5$Mu[m,j]^(test.data[i,j])*(1-mod8.5$Mu[m,j])^(1-test.data[i,j])
-      pX.9[j] <- mod9.5$Mu[m,j]^(test.data[i,j])*(1-mod9.5$Mu[m,j])^(1-test.data[i,j])
-      pX.0[j] <- mod0.5$Mu[m,j]^(test.data[i,j])*(1-mod0.5$Mu[m,j])^(1-test.data[i,j])
+    #Loop until Log-Likelihood Difference <=1
+    converge <- 500
+    while(converge>1){
+      
+      #M-Step Loop
+      pi.mat <- matrix(0,M,1)
+      mu.mat <- matrix(0,M,d)
+      for(m in 1:M){
+        pi.mat[m,1] <- (sum(gam.mat[,m])+1)/(N+M)
+        mu.mat[m,] <- (t(digit.train)%*%gam.mat[,m]+1)/(sum(gam.mat[,m])+2)
       }
-    Digit.mat[1,m] <- mod1.5$Pi[m]*prod(pX.1)
-    Digit.mat[2,m] <- mod2.5$Pi[m]*prod(pX.2)
-    Digit.mat[3,m] <- mod3.5$Pi[m]*prod(pX.3)
-    Digit.mat[4,m] <- mod4.5$Pi[m]*prod(pX.4)
-    Digit.mat[5,m] <- mod5.5$Pi[m]*prod(pX.5)
-    Digit.mat[6,m] <- mod6.5$Pi[m]*prod(pX.6)
-    Digit.mat[7,m] <- mod7.5$Pi[m]*prod(pX.7)
-    Digit.mat[8,m] <- mod8.5$Pi[m]*prod(pX.8)
-    Digit.mat[9,m] <- mod9.5$Pi[m]*prod(pX.9)
-    Digit.mat[10,m] <- mod0.5$Pi[m]*prod(pX.0)
+      
+      #E-Step Loop
+      gam.mat <- matrix(0,N,M)
+      gam.tmp <- matrix(0,N,M)
+      gam.exp <- matrix(0,N,M)
+      for(i in 1:N){
+        #Calculate Bernoulli P(X_i|mu_m)
+        for(m in 1:M){
+          pX.im <- vector()
+          for(j in 1:d){pX.im[j] <- (mu.mat[m,j])^(digit.train[i,j]*1)*(1-mu.mat[m,j])^(1-digit.train[i,j]*1)}
+          gam.tmp[i,m] <- log(pi.mat[m]*prod(pX.im))
+        }
+        
+        #Find the max pi_m*P(X_i|mu_m) and calculate the new gamma probability
+        l.max <- max(gam.tmp[i,])
+        for(m in 1:M){gam.exp[i,m] <- exp(gam.tmp[i,m]-l.max)}
+        for(m in 1:M){gam.mat[i,] <- gam.exp[i,]/sum(gam.exp[i,])}
+      }
+      
+      #Log-Likelihood
+      Q.mat <- matrix(0,N,M)
+      for(i in 1:N){
+        for(m in 1:M){
+          Q.mat[i,m] <- gam.mat[i,m]*(log(pi.mat[m])+t(digit.train[i,])%*%log(mu.mat[m,])+t(1-digit.train[i,])%*%log(1-mu.mat[m,]))
+        }
+      }
+      
+      iter <- rbind(iter,c(max(iter[,1]+1),sum(Q.mat)+sum(log(pi.mat))+log(factorial(2*M-1))+sum(log(mu.mat[m,]))+sum(log(1-mu.mat[m,]))+400*log(6)))
+      
+      #Converges when Log-Likelihood Difference <1
+      converge <- iter[max(iter[,1])+1,2]-iter[max(iter[,1]),2]
+    }
+    #Model Output - Iterations, Pi, Mu, Gamma Parameters
+    Mod.list[[digit]] <- list(Iter=iter,Pi=pi.mat,Mu=mu.mat,Gamma=gam.mat)
   }
-  Test.mat[i,1] <- sum(Digit.mat[1,])
-  Test.mat[i,2] <- sum(Digit.mat[2,])
-  Test.mat[i,3] <- sum(Digit.mat[3,])
-  Test.mat[i,4] <- sum(Digit.mat[4,])
-  Test.mat[i,5] <- sum(Digit.mat[5,])
-  Test.mat[i,6] <- sum(Digit.mat[6,])
-  Test.mat[i,7] <- sum(Digit.mat[7,])
-  Test.mat[i,8] <- sum(Digit.mat[8,])
-  Test.mat[i,9] <- sum(Digit.mat[9,])
-  Test.mat[i,10] <- sum(Digit.mat[10,])
-  Test.mat[i,12] <- which(Test.mat[i,1:10]==max(Test.mat[i,1:10]))
+  
+  #Test Data Classification
+  Test.mat <- matrix(0,ntest*10,12)
+  colnames(Test.mat) <- c("1","2","3","4","5","6","7","8","9","0","True","Test")
+  Test.mat[,11] <- testY
+  for(i in 1:(ntest*10)){
+    Digit.mat <- matrix(0,10,5)
+    for(m in 1:5){
+      pX.1 <- vector(); pX.2 <- vector(); pX.3 <- vector(); pX.4 <- vector(); pX.5 <- vector()
+      pX.6 <- vector(); pX.7 <- vector(); pX.8 <- vector(); pX.9 <- vector(); pX.0 <- vector()
+      for(j in 1:d){
+        pX.1[j] <- Mod.list[[1]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[1]]$Mu[m,j])^(1-testX[i,j])
+        pX.2[j] <- Mod.list[[2]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[2]]$Mu[m,j])^(1-testX[i,j])
+        pX.3[j] <- Mod.list[[3]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[3]]$Mu[m,j])^(1-testX[i,j])
+        pX.4[j] <- Mod.list[[4]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[4]]$Mu[m,j])^(1-testX[i,j])
+        pX.5[j] <- Mod.list[[5]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[5]]$Mu[m,j])^(1-testX[i,j])
+        pX.6[j] <- Mod.list[[6]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[6]]$Mu[m,j])^(1-testX[i,j])
+        pX.7[j] <- Mod.list[[7]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[7]]$Mu[m,j])^(1-testX[i,j])
+        pX.8[j] <- Mod.list[[8]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[8]]$Mu[m,j])^(1-testX[i,j])
+        pX.9[j] <- Mod.list[[9]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[9]]$Mu[m,j])^(1-testX[i,j])
+        pX.0[j] <- Mod.list[[10]]$Mu[m,j]^(testX[i,j])*(1-Mod.list[[10]]$Mu[m,j])^(1-testX[i,j])
+      }
+      Digit.mat[1,m] <- Mod.list[[1]]$Pi[m]*prod(pX.1)
+      Digit.mat[2,m] <- Mod.list[[2]]$Pi[m]*prod(pX.2)
+      Digit.mat[3,m] <- Mod.list[[3]]$Pi[m]*prod(pX.3)
+      Digit.mat[4,m] <- Mod.list[[4]]$Pi[m]*prod(pX.4)
+      Digit.mat[5,m] <- Mod.list[[5]]$Pi[m]*prod(pX.5)
+      Digit.mat[6,m] <- Mod.list[[6]]$Pi[m]*prod(pX.6)
+      Digit.mat[7,m] <- Mod.list[[7]]$Pi[m]*prod(pX.7)
+      Digit.mat[8,m] <- Mod.list[[8]]$Pi[m]*prod(pX.8)
+      Digit.mat[9,m] <- Mod.list[[9]]$Pi[m]*prod(pX.9)
+      Digit.mat[10,m] <- Mod.list[[10]]$Pi[m]*prod(pX.0)
+    }
+    Test.mat[i,1] <- sum(Digit.mat[1,])
+    Test.mat[i,2] <- sum(Digit.mat[2,])
+    Test.mat[i,3] <- sum(Digit.mat[3,])
+    Test.mat[i,4] <- sum(Digit.mat[4,])
+    Test.mat[i,5] <- sum(Digit.mat[5,])
+    Test.mat[i,6] <- sum(Digit.mat[6,])
+    Test.mat[i,7] <- sum(Digit.mat[7,])
+    Test.mat[i,8] <- sum(Digit.mat[8,])
+    Test.mat[i,9] <- sum(Digit.mat[9,])
+    Test.mat[i,10] <- sum(Digit.mat[10,])
+    Test.mat[i,12] <- which(Test.mat[i,1:10]==max(Test.mat[i,1:10]))
+  }
+  
+  #Classification Error Rate
+  Test.mat <- cbind(Test.mat,abs(sign(Test.mat[,11]-Test.mat[,12])))
+  Error.Rate <- sum(Test.mat[,13])/(ntest*10)
+  
+  #Output - Model List
+  return(list(Mod.list=Mod.list,Error.Rate=Error.Rate))
 }
-Sys.time()
-
-#Classification Error Rate; 11.26% Error Rate
-Test.mat <- cbind(Test.mat,abs(sign(Test.mat[,11]-Test.mat[,12])))
-Error.Rate <- sum(Test.mat[,13])/(num.test*10)
